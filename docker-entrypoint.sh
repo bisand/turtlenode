aea1b908fb18d749f71d0f476302634376b7bdd2
André Biseth <andre@biseth.net>
2021-04-19 00:52:47 +0200

    More reordering

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -29,14 +29,14 @@ if [[ "$1" = 'ttyd' ]]; then
             --p2p-external-port ${P2P_EXTERNAL_PORT} \
             --rpc-bind-ip ${RPC_BIND_IP} \
             --rpc-bind-port ${RPC_BIND_PORT} \
+            --fee-amount ${FEE_AMOUNT} \
+            --fee-address ${FEE_ADDRESS} \
             --add-exclusive-node ${ADD_EXCLUSIVE_NODE} \
             --seed-node ${SEED_NODE} \
             --add-peer ${ADD_PEER} \
             --add-priority-node ${ADD_PRIORITY_NODE} \
             --enable-blockexplorer ${ENABLE_BLOCKEXPLORER} \
-            --enable-cors ${ENABLE_CORS} \
-            --fee-amount ${FEE_AMOUNT} \
-            --fee-address ${FEE_ADDRESS}
+            --enable-cors ${ENABLE_CORS} 
 
     if [[ "$DB_ENABLE_COMPRESSION" = true ]]; then
         set -- "$@" --db-enable-compression
