ca9af3f9229ce016e6316a13bdb47a8bcad8381f
André Biseth <andre@biseth.net>
2021-04-22 21:41:25 +0200

    Moved params

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,14 +9,11 @@ if [[ "$1" = 'TurtleCoind' ]]; then
             --db-read-buffer-size ${DB_READ_BUFFER_SIZE} \
             --db-threads ${DB_THREADS} \
             --db-write-buffer-size ${DB_WRITE_BUFFER_SIZE} \
-            --allow-local-ip ${ALLOW_LOCAL_IP} \
-            --hide-my-port ${HIDE_MY_PORT} \
             --p2p-bind-ip ${P2P_BIND_IP} \
             --p2p-bind-port ${P2P_BIND_PORT} \
             --p2p-external-port ${P2P_EXTERNAL_PORT} \
             --rpc-bind-ip ${RPC_BIND_IP} \
             --rpc-bind-port ${RPC_BIND_PORT} \
-            --enable-blockexplorer ${ENABLE_BLOCKEXPLORER} \
             --fee-amount ${FEE_AMOUNT} \
             --fee-address ${FEE_ADDRESS} \
             --add-exclusive-node ${ADD_EXCLUSIVE_NODE} \
@@ -24,6 +21,18 @@ if [[ "$1" = 'TurtleCoind' ]]; then
             --add-peer ${ADD_PEER} \
             --add-priority-node ${ADD_PRIORITY_NODE} \
             --enable-cors ${ENABLE_CORS} "$@" 
+
+    if [[ "$ALLOW_LOCAL_IP" = true ]]; then
+        set -- "$@" --allow-local-ip
+    fi
+
+    if [[ "$HIDE_MY_PORT" = true ]]; then
+        set -- "$@" --hide-my-port
+    fi
+
+    if [[ "$ENABLE_BLOCKEXPLORER" = true ]]; then
+        set -- "$@" --enable-blockexplorer
+    fi
 
     if [[ "$DB_ENABLE_COMPRESSION" = true ]]; then
         set -- "$@" --db-enable-compression
